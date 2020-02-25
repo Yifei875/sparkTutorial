33731624412116a49441d60b050dfb59bf0b4e82
Yifei <y_ren@apple.com>
2020-02-25 14:44:38 -0800

    complete main function for AirportsByLatitudeProblem.java

--- src/main/java/com/sparkTutorial/rdd/airports/AirportsByLatitudeProblem.java
+++ src/main/java/com/sparkTutorial/rdd/airports/AirportsByLatitudeProblem.java
@@ -1,5 +1,11 @@
 package com.sparkTutorial.rdd.airports;
 
+import com.sparkTutorial.rdd.commons.Utils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
 public class AirportsByLatitudeProblem {
 
     public static void main(String[] args) throws Exception {
@@ -16,5 +22,18 @@ public class AirportsByLatitudeProblem {
            "Tofino", 49.082222
            ...
          */
+        SparkConf sparkConf = new SparkConf().setAppName("airports").setMaster("local[3]");
+
+        JavaSparkContext sc = new JavaSparkContext(sparkConf);
+
+        JavaRDD<String> airports = sc.textFile("in/airports.text");
+
+        JavaRDD<String> airportsInUSA = airports.filter(line -> Float.valueOf(line.split(Utils.COMMA_DELIMITER)[6])>40);
+
+        JavaRDD<String> airportsNameAndCityName = airportsInUSA.map(line -> {
+            String[] splits = line.split(Utils.COMMA_DELIMITER);
+            return StringUtils.join(new String[]{splits[1], splits[6]}, ",");
+        });
+        airportsNameAndCityName.saveAsTextFile("out/airports_by_latitude.text");
     }
 }
